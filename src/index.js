237981334e48c2a3f1d297ae7998ce1e43499cc7
Felipe Belchior <felipeoliveira.b@gmail.com>
2024-08-10 14:04:13 -0300

    v 1.6

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 require("./app/controllers/controller")(app);
 //require("./app/controllers/controller_sobre")(app);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
