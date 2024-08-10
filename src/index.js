f627b62694379291b50369f73903c4993c2e3c27
Felipe Belchior <felipeoliveira.b@gmail.com>
2024-08-09 22:11:18 -0300

    v 1.5

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:true, limit:'50mb'}))
 app.use(cors());
 require("./app/controllers/controller")(app);
-require("./app/controllers/controller_sobre")(app);
+//require("./app/controllers/controller_sobre")(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
