f41b506726965a60b64376175faea279f0bbfefb
Felipe Belchior <felipeoliveira.b@gmail.com>
2024-08-10 14:50:45 -0300

    v 1.6

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://articusarname:Z1tpN37LxwFokVKm@articcluster.vnr4w.mongodb.net/');
+mongoose.connect('mongodb+srv://articusarname:<Iron-man1573594646>@articcluster.vnr4w.mongodb.net/');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
